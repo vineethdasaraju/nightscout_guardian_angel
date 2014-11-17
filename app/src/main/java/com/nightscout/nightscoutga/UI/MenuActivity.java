9f28efbd0c73dc2c013038d9828b0757299388b0
pradeeppeesarla <pradeep.peesarla@gmail.com>
2014-11-17 04:12:26 -0500

    MenuActivity added

---
+++ app/src/main/java/com/nightscout/nightscoutga/UI/MenuActivity.java
@@ -0,0 +1,18 @@
+package com.nightscout.nightscoutga.UI;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.nightscout.nightscoutga.R;
+
+/**
+ * Created by User on 17/11/2014.
+ */
+public class MenuActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.menu);
+    }
+}
