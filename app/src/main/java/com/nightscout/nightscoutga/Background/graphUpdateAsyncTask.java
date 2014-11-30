7df50e7ed32565ab87369c09cb356100c93db1ca
pradeeppeesarla <pradeep.peesarla@gmail.com>
2014-11-30 00:38:00 -0500

    profile updated

---
+++ app/src/main/java/com/nightscout/nightscoutga/Background/graphUpdateAsyncTask.java
@@ -0,0 +1,7 @@
+package com.nightscout.nightscoutga.Background;
+
+/**
+ * Created by User on 28/11/2014.
+ */
+public class graphUpdateAsyncTask {
+}
--- app/src/main/java/com/nightscout/nightscoutga/UI/Fragments/MyProfileFragment.java
+++ app/src/main/java/com/nightscout/nightscoutga/UI/Fragments/MyProfileFragment.java
@@ -1,15 +1,9 @@
-package com.nightscout.nightscoutga.UI;
+package com.nightscout.nightscoutga.UI.Fragments;
 
 import android.app.Activity;
-import android.content.Context;
-import android.content.SharedPreferences;
 import android.net.Uri;
 import android.os.Bundle;
 import android.app.Fragment;
-import android.preference.Preference;
-import android.preference.PreferenceFragment;
-import android.preference.PreferenceManager;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -19,24 +13,22 @@ import com.nightscout.nightscoutga.R;
 /**
  * A simple {@link Fragment} subclass.
  * Activities that contain this fragment must implement the
- * {@link SettingsFragment.OnFragmentInteractionListener} interface
+ * {@link MyProfileFragment.OnFragmentInteractionListener} interface
  * to handle interaction events.
- * Use the {@link SettingsFragment#newInstance} factory method to
+ * Use the {@link MyProfileFragment#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class SettingsFragment extends PreferenceFragment implements SharedPreferences.OnSharedPreferenceChangeListener{
+public class MyProfileFragment extends Fragment {
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    //private static final String ARG_PARAM1 = "param1";
-    //private static final String ARG_PARAM2 = "param2";
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
 
     // TODO: Rename and change types of parameters
-    //private String mParam1;
-    //private String mParam2;
+    private String mParam1;
+    private String mParam2;
 
     private OnFragmentInteractionListener mListener;
-
-    Context con;
 
     /**
      * Use this factory method to create a new instance of
@@ -44,57 +36,37 @@ public class SettingsFragment extends PreferenceFragment implements SharedPrefer
      *
      * @param param1 Parameter 1.
      * @param param2 Parameter 2.
-     * @return A new instance of fragment SettingsFragment.
+     * @return A new instance of fragment MyProfileFragment.
      */
     // TODO: Rename and change types and number of parameters
-    public static SettingsFragment newInstance(String param1, String param2) {
-        SettingsFragment fragment = new SettingsFragment(null);
+    public static MyProfileFragment newInstance(String param1, String param2) {
+        MyProfileFragment fragment = new MyProfileFragment();
         Bundle args = new Bundle();
-        //args.putString(ARG_PARAM1, param1);
-        //args.putString(ARG_PARAM2, param2);
+        args.putString(ARG_PARAM1, param1);
+        args.putString(ARG_PARAM2, param2);
         fragment.setArguments(args);
         return fragment;
     }
 
-    public SettingsFragment(Context con) {
+    public MyProfileFragment() {
         // Required empty public constructor
-        this.con = con;
     }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        Log.e("FRA", "Hello");
-        //if (getArguments() != null) {
-        //    mParam1 = getArguments().getString(ARG_PARAM1);
-        //    mParam2 = getArguments().getString(ARG_PARAM2);
-        //}
-        addPreferencesFromResource(R.xml.activity_settings);
-        getPreferenceScreen().getSharedPreferences()
-                .registerOnSharedPreferenceChangeListener(this);
-    }
-
-    @Override
-    public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String key) {
-        Log.e("CHGD", "Hello");
-        if (key.equals("sync_time_list")) {
-            Preference connectionPref = new Preference(con);
-            connectionPref.setKey(key);
-            //findPreference(key);
-            // Set summary to be the user-description for the selected value
-            connectionPref.setSummary(sharedPreferences.getString(key, "") + " minutes");
-            SharedPreferences sharedPref = PreferenceManager.getDefaultSharedPreferences(con);
-            String syncConnPref = sharedPref.getString("sync_time_list", "");
-            Log.e("CHGD", syncConnPref);
+        if (getArguments() != null) {
+            mParam1 = getArguments().getString(ARG_PARAM1);
+            mParam2 = getArguments().getString(ARG_PARAM2);
         }
     }
 
-    /*@Override
+    @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.activity_settings, container, false);
-    }*/
+        return inflater.inflate(R.layout.fragment_my_profile, container, false);
+    }
 
     // TODO: Rename method, update argument and hook method into UI event
     public void onButtonPressed(Uri uri) {
