a6e558e8b4725065ff9d9a958696b8f82a65ddbc
pradeeppeesarla <pradeep.peesarla@gmail.com>
2014-12-01 00:56:00 -0500

    Updated Glucose AsyncTask

---
+++ app/src/main/java/com/nightscout/nightscoutga/Models/Glucose.java
@@ -0,0 +1,42 @@
+package com.nightscout.nightscoutga.Models;
+
+//import com.nightscout.android.dexcom.Constants;
+
+/**
+ * Created by User on 30/11/2014.
+ */
+public class Glucose {
+
+    int patientId;
+    String date;
+    String time;
+    int glucose;
+
+    /*public Glucose(int g, Date d){
+        this.date = DateFormat.getDateInstance().format(date);
+        this.glucose = g;
+        this.time = DateFormat.getTimeInstance().format(date);
+        this.patientId = Integer.parseInt(Constants.userid);
+    }*/
+
+    public Glucose(int pid, int g, String d, String time){
+        this.patientId = pid;
+        this.date = d;
+        this.glucose = g;
+        this.time = time;
+        //this.patientId = Integer.parseInt(Constants.userid);
+    }
+
+    public String getGlucoseDate(){
+        return date;
+    }
+
+    public String getGlucoseTime(){
+        return time;
+    }
+
+    public int getGlucoseValue(){
+        return glucose;
+    }
+
+}
